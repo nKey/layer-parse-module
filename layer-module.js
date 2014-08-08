1689f47ff0e8e5bd253579d8b146ec254c3c0d75
Meera Parat <meera.parat@gmail.com>
2014-08-08 12:54:23 -0700

    Update layer-module.js

--- layer-module.js
+++ layer-module.js
@@ -17,14 +17,14 @@ module.exports = {
    },
 
    layerIdentityToken: function(userID, nonce) {
-		  var header =  JSON.stringify({
+		var header =  JSON.stringify({
           typ: "JWS", // Expresses a MIME Type of application/JWS
           alg: "RS256", // Expresses the type of algorithm used to sign the token, must be RS256
           cty: "layer-eit;v=1", // Express a Content Type of application/layer-eit;v=1
           kid: this.layerKeyID,
         });
 
-		  var currentTimeInSeconds = Math.round(new Date() / 1000);
+		var currentTimeInSeconds = Math.round(new Date() / 1000);
     	var expirationTime = currentTimeInSeconds + 10000;
 
     	var claim = JSON.stringify({

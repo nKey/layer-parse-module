70f34f97d8cb195be5081757636b81db9fc01252
Meera Parat <meera.parat@gmail.com>
2014-08-08 12:54:51 -0700

    Update layer-module.js

--- layer-module.js
+++ layer-module.js
@@ -11,20 +11,20 @@ module.exports = {
     if (!keyID) throw new Error('Missing keyID parameter');
     if (!key) throw new Error('Missing private key parameter');
     this.layerProviderID = providerID;
-  	this.layerKeyID = keyID;
-  	this.privateKey = key;
-  	return this;
+    this.layerKeyID = keyID;
+    this.privateKey = key;
+    return this;
    },
 
    layerIdentityToken: function(userID, nonce) {
-		var header =  JSON.stringify({
+	var header =  JSON.stringify({
           typ: "JWS", // Expresses a MIME Type of application/JWS
           alg: "RS256", // Expresses the type of algorithm used to sign the token, must be RS256
           cty: "layer-eit;v=1", // Express a Content Type of application/layer-eit;v=1
           kid: this.layerKeyID,
         });
 
-		var currentTimeInSeconds = Math.round(new Date() / 1000);
+	var currentTimeInSeconds = Math.round(new Date() / 1000);
     	var expirationTime = currentTimeInSeconds + 10000;
 
     	var claim = JSON.stringify({

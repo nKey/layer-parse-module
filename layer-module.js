a4591f778c11347fbfbceb82dedd0181259def32
Neil Mehta <dwolf07@users.noreply.github.com>
2015-06-05 15:56:12 -0700

    Fixed mime type

--- layer-module.js
+++ layer-module.js
@@ -18,7 +18,7 @@ module.exports = {
 
    layerIdentityToken: function(userID, nonce) {
 	var header =  JSON.stringify({
-          typ: "JWS", // Expresses a MIME Type of application/JWS
+          typ: "JWT", // Expresses a MIME Type of application/JWT
           alg: "RS256", // Expresses the type of algorithm used to sign the token, must be RS256
           cty: "layer-eit;v=1", // Express a Content Type of application/layer-eit;v=1
           kid: this.layerKeyID,
